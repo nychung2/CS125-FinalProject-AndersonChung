f9d8c34860d38a25d671b2db26d09dea4562e5b3
Nathan Chung <nychung2@illinois.edu>
2020-04-28 00:23:33 -0500

    4/27 commit

--- app/src/main/java/com/example/cs125finalproject_andersonchung/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject_andersonchung/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cs125finalproject_andersonchung;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,40 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //handle the Breed Change Button/RadioGroup list
+        Button changeBreed = findViewById(R.id.changeBreed);
+        changeBreed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("changeBreed", "Change Breed Clicked");
+            }
+        });
+
+        //handle the next picture
+        Button next = findViewById(R.id.nextDog);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("nextDog", "Next picture requested");
+                updatePicture();
+            }
+        });
+
+        //handle downloading to phone
+        Button download = findViewById(R.id.saveDog);
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("saveDog", "User Tried Downloading");
+            }
+        });
+    }
+    protected void updatePicture() {
+        // handle the incoming image from DogApi
+        ImageView picture = findViewById(R.id.dogView);
     }
 }
